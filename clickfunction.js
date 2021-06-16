5427e3b14103cdeee579461cb66ff1e9d5a5f8c9
Embox <71951742+emboxity@users.noreply.github.com>
2021-06-15 22:31:54 -0400

    Shows app/form detail when submitted.

--- clickfunction.js
+++ clickfunction.js
@@ -1,9 +1,29 @@
+const btn = document.querySelector(".button");
+
+const firstName = document.querySelector(".first-name");
+const lastName = document.querySelector(".last-name");
+
+const first = document.querySelector(".first");
+const last = document.querySelector(".last");
+
+const userAgeInfo = document.querySelector(".user-age")
+const age = document.querySelector(".userAgeInfo")
+
+
+btn.addEventListener("click", () => {
+  first.textContent = `First name:${firstName.value}`;
+  last.textContent = `First name:${lastName.value}`;
+  age.textContent = `Age:${userAgeInfo.value}`;
+
+});
+
 function submitFuncton() {
-    var x = document.getElementById("frm1");
-    var text = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-      text += x.elements[i].value + "<br>";
-    }
-    document.getElementById("checkInfo").innerHTML = text + "Is this all your information?";
+  var x = document.getElementById("frm1");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length; i++) {
+  
   }
+  document.getElementById("checkInfo").innerHTML =
+    text + "Is this all your information?";
+}

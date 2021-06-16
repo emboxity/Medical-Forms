bdca5950d0a38130c2151e6004fbb58cd5ce375e
Embox <71951742+emboxity@users.noreply.github.com>
2021-06-16 11:40:16 -0400

    Added functionality for the newer questions

--- clickfunction.js
+++ clickfunction.js
@@ -9,12 +9,22 @@ const last = document.querySelector(".last");
 const userAgeInfo = document.querySelector(".user-age")
 const age = document.querySelector(".userAgeInfo")
 
+const userSexInfo = document.querySelector(".user-sex")
+const sex = document.querySelector(".userSexInfo")
+
+const userEmail = document.querySelector(".user-email")
+const email = document.querySelector(".email")
+
+const userPhone = document.querySelector(".user-number")
+const phone = document.querySelector(".number")
 
 btn.addEventListener("click", () => {
   first.textContent = `First name:${firstName.value}`;
   last.textContent = `First name:${lastName.value}`;
   age.textContent = `Age:${userAgeInfo.value}`;
-
+  sex.textContent = `Sex: ${userSexInfo.value}`
+  email.textContent = `Email: ${userEmail.value}`
+  phone.textContent = `Phone Number: ${userPhone.value}`
 });
 
 function submitFuncton() {

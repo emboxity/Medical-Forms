da6455957ba278bf27c9d58faadbc0aa348b5ff8
Embox <71951742+emboxity@users.noreply.github.com>
2021-06-17 15:11:04 -0400

    Added the JS code for the disclaimer to work

---
+++ disclaimerfunction.js
@@ -0,0 +1,8 @@
+function disclaimerFunction(){
+const dbtn = document.querySelector(".dbutton");
+
+var disc = document.getElementById("dInfo")
+
+disc.innerHTML += `All content found on the website, including: text, images, audio, or other formats were created for informational purposes only, and is not a proper diagnosis. The content is not intended to be a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition. Never disregard professional medical advice or delay in seeking it because of something you have read on this website.
+If you think you may have a medical emergency, call your doctor, go to the emergency department, or call 911 immediately. We do not recommend or endorse any specific tests, physicians, products, procedures, opinions, or other information that may be mentioned on here. Reliance on any information provided by the site is at your own risk.`;
+}

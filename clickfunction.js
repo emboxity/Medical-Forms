795b0758ea0449c6f1dd515e6824f96187fd0004
Embox <71951742+emboxity@users.noreply.github.com>
2021-06-16 20:37:27 -0400

    Code refactoring, and added a required questions

--- clickfunction.js
+++ clickfunction.js
@@ -1,3 +1,4 @@
+function submitFuncton() {
 const btn = document.querySelector(".button");
 
 const firstName = document.querySelector(".first-name");
@@ -18,22 +19,31 @@ const email = document.querySelector(".email")
 const userPhone = document.querySelector(".user-number")
 const phone = document.querySelector(".number")
 
-btn.addEventListener("click", () => {
-  first.textContent = `First name:${firstName.value}`;
-  last.textContent = `First name:${lastName.value}`;
-  age.textContent = `Age:${userAgeInfo.value}`;
-  sex.textContent = `Sex: ${userSexInfo.value}`
-  email.textContent = `Email: ${userEmail.value}`
-  phone.textContent = `Phone Number: ${userPhone.value}`
-});
-
-function submitFuncton() {
   var x = document.getElementById("frm1");
   var text = "";
-  var i;
-  for (i = 0; i < x.length; i++) {
   
+  var a = document.forms["frm1"]["fname"].value;
+  var b = document.forms["frm1"]["lname"].value;
+  var c = document.forms["frm1"]["uage"].value;
+  var d = document.forms["frm1"]["usex"].value;
+  var e = document.forms["frm1"]["uemail"].value;
+  var f = document.forms["frm1"]["unumber"].value;
+
+  if (a || b || c || d || e || f == "") {
+    alert("You are missing required information!");
+    return;
+  } else {
+      
+  btn.addEventListener("click", () => {
+    first.textContent = `First name:${firstName.value}`;
+    last.textContent = `Last name:${lastName.value}`;
+    age.textContent = `Age:${userAgeInfo.value}`;
+    sex.textContent = `Sex: ${userSexInfo.value}`
+    email.textContent = `Email: ${userEmail.value}`
+    phone.textContent = `Phone Number: ${userPhone.value}`
+    
+  });
+
+  document.getElementById("checkInfo").innerHTML = text + "Is this all your information?";
   }
-  document.getElementById("checkInfo").innerHTML =
-    text + "Is this all your information?";
 }
